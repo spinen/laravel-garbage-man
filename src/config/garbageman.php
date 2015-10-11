effc47330e4525c0949d12323a2edb822ca0a19b
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-10-11 09:13:23 -0400

    Adding configs for logging

--- src/config/garbageman.php
+++ src/config/garbageman.php
@@ -4,6 +4,35 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Level to log
+    |--------------------------------------------------------------------------
+    |
+    | The level that log messages are generated, which will display information
+    | on the console output and in the logs.
+    |
+    |       0       Emergency: system is unusable
+    |       1       Alert: action must be taken immediately
+    |       2       Critical: critical conditions
+    |       3       Error: error conditions
+    |       4       Warning: warning conditions
+    |       5       Notice: normal but significant condition
+    |       6       Info: informational messages
+    |       7       Debug: debug - level messages
+    |
+    | There is a key for the console & one for the log.  Here is an example...
+    |
+    |   'logging_level' => [
+    |       'console' => 3,
+    |       'log'     => 6,
+    |   ],
+    */
+    'logging_level' => [
+        'console' => 6,
+        'log'     => 6,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
     | Models & age to allow the soft deleted record to stay.
     |--------------------------------------------------------------------------
     |
@@ -17,7 +46,7 @@ return [
     | This would purge any ModelOne's, that were deleted over 14 days ago and any
     | ModelTwo's that are were deleted over 30 days ago.
     */
-    'schedule' => [
+    'schedule'      => [
         //
     ],
 
