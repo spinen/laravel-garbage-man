23ee99cc45871aa021b327a0f46c4b649e5b3dae
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-10-10 20:28:00 -0400

    Setting the configs based on what is needed over the template

--- src/config/garbageman.php
+++ src/config/garbageman.php
@@ -4,79 +4,21 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Filter type
+    | Models & age to allow the soft deleted record to stay.
     |--------------------------------------------------------------------------
     |
-    | The filter is to apply blocking ("block") or allowing ("allow") strategy
-    | using the rules defined below.
+    | The age is in days for each model.  Here is an example...
     |
+    |   schedule' => [
+    |       App\ModelOne::class => 14,
+    |       App\ModelTwo::class => 30,
+    |   ],
+    |
+    | This would purge any ModelOne's, that were deleted over 14 days ago and any
+    | ModelTwo's that are were deleted over 30 days ago.
     */
-    'type'    => 'block',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Blocked devices, browsers and/or versions
-    |--------------------------------------------------------------------------
-    |
-    | This array defines the items to be filtered out when a request is made
-    | to all routes.  There is a three level structure to the array where it
-    | goes device -> browser -> version.  The device string can be "Mobile",
-    | "Other", or "Tablet" as defined by UAParser\Parser. You can define an
-    | array of specific browser names that you are targeting or use "*" to
-    | block all browsers of that device type.  At the final level, you can
-    | define an array of comparison operators to use for that specific browser
-    | or you can use a "*" to block all versions of that browser.  We are
-    | using php's version_compare function...
-    |
-    |      @link http://php.net/manual/en/function.version-compare.php
-    |
-    | so you can see the operations documented there.
-    |
-    | Here is an example...
-    |
-    | 'rules' => [
-    |     'Mobile' => '*',
-    |     'Other'  => [
-    |         'IE'    => '*',
-    |     ],
-    |     'Tablet' => [
-    |         'Opera' => [
-    |             '<' => '6',
-    |         ]
-    |     ],
-    | ],
-    |
-    | In this example, we are allowing/blocking the following...
-    |
-    |     * All mobile devices
-    |     * All versions of IE that is not on a tablet
-    |     * Any version of Opera less than 6 on a tablet
-    |
-    */
-    'rules'   => [
-        'Mobile' => [],
-        'Other'  => [],
-        'Tablet' => [],
+    'schedule' => [
+        //
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Blocked devices, browsers and/or versions
-    |--------------------------------------------------------------------------
-    |
-    | The name of the route to redirect the user to if the browser is blocked
-    |
-    */
-    'route'   => 'incompatible_browser',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Duration to cache the browser as being blocked
-    |--------------------------------------------------------------------------
-    |
-    | The time in minutes to cache that the client is blocked
-    |
-    */
-    'timeout' => 3600,
 
 ];
